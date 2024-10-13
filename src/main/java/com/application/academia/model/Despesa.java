96c304ad26586927307b8c7a6f901d1b2d05893b
marcela-cruz <marcela.c.santos.cruz@gmail.com>
2024-10-12 23:11:24 -0300

    Create Despesa.java

---
+++ src/main/java/com/application/academia/model/Despesa.java
@@ -0,0 +1,44 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.application.academia.model;
+
+/**
+ *
+ * @author marce
+ */
+public class Despesa {
+    
+    private String descricao;
+    private double valor;
+
+    public Despesa() {
+    }
+
+    public Despesa(String descricao, double valor) {
+        this.descricao = descricao;
+        this.valor = valor;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public double getValor() {
+        return valor;
+    }
+
+    public void setValor(double valor) {
+        this.valor = valor;
+    }
+
+    @Override
+    public String toString() {
+        return "Despesa{" + "descricao=" + descricao + ", valor=" + valor + '}';
+    }
+}
