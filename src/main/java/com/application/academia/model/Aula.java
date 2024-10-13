22f81012d487357ee9c09136d07e7de1de5e0172
marcela-cruz <marcela.c.santos.cruz@gmail.com>
2024-10-12 23:11:26 -0300

    Update Aula.java

--- src/main/java/com/application/academia/model/Aula.java
+++ src/main/java/com/application/academia/model/Aula.java
@@ -11,6 +11,54 @@ package com.application.academia.model;
 public class Aula {
     
     private String tipo; // spinning, musculação, fit dance e pilates
-    private String sala; // spinning, musculação, fit dance e pilates
+    private String sala;
     private double valor;
+    private Instrutor instrutor;
+
+    public Aula() {
+    }
+
+    public Aula(String tipo, String sala, double valor, Instrutor instrutor) {
+        this.tipo = tipo;
+        this.sala = sala;
+        this.valor = valor;
+        this.instrutor = instrutor;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public String getSala() {
+        return sala;
+    }
+
+    public void setSala(String sala) {
+        this.sala = sala;
+    }
+
+    public double getValor() {
+        return valor;
+    }
+
+    public void setValor(double valor) {
+        this.valor = valor;
+    }
+
+    public Instrutor getInstrutor() {
+        return instrutor;
+    }
+
+    public void setInstrutor(Instrutor instrutor) {
+        this.instrutor = instrutor;
+    }
+
+    @Override
+    public String toString() {
+        return "Aula{" + "tipo=" + tipo + ", sala=" + sala + ", valor=" + valor + ", instrutor=" + instrutor + '}';
+    }
 }
