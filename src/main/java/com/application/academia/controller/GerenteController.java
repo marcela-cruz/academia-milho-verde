344b676f91f516c0ac64409a3d60ccfb186e7f7f
marcela-cruz <marcela.c.santos.cruz@gmail.com>
2024-10-15 10:04:52 -0300

    Update GerenteController.java

--- src/main/java/com/application/academia/controller/GerenteController.java
+++ src/main/java/com/application/academia/controller/GerenteController.java
@@ -4,10 +4,60 @@
  */
 package com.application.academia.controller;
 
+import com.application.academia.model.Despesa;
+import com.application.academia.model.Receita;
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  *
  * @author marce
  */
 public class GerenteController {
     
+    private List<Despesa> despesas;
+    private List<Receita> receitas;
+
+    public GerenteController() {
+    }
+
+    public GerenteController(List<Despesa> despesas, List<Receita> receitas) {
+        this.despesas = new ArrayList<>();
+        this.receitas = new ArrayList<>();
+    }
+
+    // gerenciamento de funcinários  
+    public void cadastrarFuncionario() {
+
+    }
+
+    public void editarFuncionario() {
+
+    }
+
+    public void excluirFuncionario() {
+
+    }
+
+    // controle de despesas e receitas
+    public void registrarDespesas() {
+
+    }
+
+    public void registrarReceitas() {
+
+    }
+
+    public void visualizarDespesas() {
+
+    }
+
+    public void visualizarReceitas() {
+
+    }
+
+    // balanço financeiro
+    public void calcularBalancoMensal() {
+
+    }
 }
