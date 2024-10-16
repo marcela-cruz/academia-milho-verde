d75014a476069f5f003efd30e31a307e69a19347
marcela-cruz <marcela.c.santos.cruz@gmail.com>
2024-10-15 22:39:58 -0300

    Update GerenteController.java

--- src/main/java/com/application/academia/controller/GerenteController.java
+++ src/main/java/com/application/academia/controller/GerenteController.java
@@ -6,6 +6,8 @@ package com.application.academia.controller;
 
 import com.application.academia.model.Despesa;
 import com.application.academia.model.Receita;
+import com.application.academia.model.Usuario;
+import com.application.academia.service.AutenticacaoService;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -15,15 +17,23 @@ import java.util.List;
  */
 public class GerenteController {
     
+    private AutenticacaoService autenticacaoService;
+    private Usuario usuarioLogado;
     private List<Despesa> despesas;
     private List<Receita> receitas;
 
     public GerenteController() {
     }
 
-    public GerenteController(List<Despesa> despesas, List<Receita> receitas) {
+    public GerenteController(AutenticacaoService autenticacaoService, Usuario usuarioLogado, List<Despesa> despesas, List<Receita> receitas) {
+        this.autenticacaoService = autenticacaoService;
+        this.usuarioLogado = usuarioLogado;
         this.despesas = new ArrayList<>();
         this.receitas = new ArrayList<>();
+    }
+    
+    private boolean isAdmin() {
+        return usuarioLogado != null && usuarioLogado.isAdministrador();
     }
 
     // gerenciamento de funcinários  
