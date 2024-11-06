1ca971c2e6e67f5804a8cfe2d1490bd2ebec43a8
marcela-cruz <marcela.c.santos.cruz@gmail.com>
2024-11-05 22:24:36 -0300

    Update Reserva.java
    construção do modelo da classe reserva

--- src/main/java/com/application/academia/model/Reserva.java
+++ src/main/java/com/application/academia/model/Reserva.java
@@ -4,10 +4,75 @@
  */
 package com.application.academia.model;
 
+import com.application.academia.controller.GeradorId;
+
 /**
  *
  * @author marce
  */
 public class Reserva {
-    
+
+    private String id;
+    private Cliente cliente;
+    private Aula aula;
+    private StatusReserva status;
+
+    public enum StatusReserva {
+        AGENDAMENTO_PRELIMINAR, AGENDAMENTO_DEFINITIVO, CANCELADA, CONCLUIDA
+    }
+
+    public Reserva() {
+    }
+
+    public Reserva(String id, Cliente cliente, Aula aula, StatusReserva status) {
+        this.id = GeradorId.gerarId();
+        this.cliente = cliente;
+        this.aula = aula;
+        this.status = status;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public Cliente getCliente() {
+        return cliente;
+    }
+
+    public void setCliente(Cliente cliente) {
+        this.cliente = cliente;
+    }
+
+    public Aula getAula() {
+        return aula;
+    }
+
+    public void setAula(Aula aula) {
+        this.aula = aula;
+    }
+
+    public StatusReserva getStatus() {
+        return status;
+    }
+
+    public void setStatus(StatusReserva status) {
+        this.status = status;
+    }
+
+    public void confirmarReserva() {
+        if (status == StatusReserva.AGENDAMENTO_PRELIMINAR) {
+            status = StatusReserva.AGENDAMENTO_DEFINITIVO;
+        } else {
+            throw new IllegalStateException("Reserva não pode ser confirmada neste estado");
+        }
+    }
+
+    public void cancelarReserva() {
+        status = StatusReserva.CANCELADA;
+    }
+
+    @Override
+    public String toString() {
+        return "Reserva{" + "id=" + id + ", cliente=" + cliente + ", aula=" + aula + ", status=" + status + '}';
+    }
 }
