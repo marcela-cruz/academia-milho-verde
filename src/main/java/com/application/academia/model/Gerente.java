dcce6b14b7cedfd7b93024d93501205c719d5dbd
marcela-cruz <marcela.c.santos.cruz@gmail.com>
2024-11-05 22:25:40 -0300

    Update Gerente.java
    construção do modelo gerente

--- src/main/java/com/application/academia/model/Gerente.java
+++ src/main/java/com/application/academia/model/Gerente.java
@@ -8,6 +8,36 @@ package com.application.academia.model;
  *
  * @author marce
  */
-public class Gerente {
-    
+public class Gerente extends Funcionario {
+
+    /**
+     * Construtor sem argumentos. Inicializa o gerente com valores padrão (se
+     * houver).
+     */
+    public Gerente() {
+    }
+
+    /**
+     *
+     * @param nome
+     * @param dataNascimento
+     * @param idade
+     * @param salario
+     * @param login
+     * @param senha
+     * @param tipo
+     */
+    public Gerente(String nome, String dataNascimento, int idade, double salario, String login, String senha, TipoUsuario tipo) {
+        super(nome, dataNascimento, idade, salario, login, senha, tipo);
+    }
+
+    /**
+     * Retorna uma representação em string do objeto Gerente.
+     *
+     * @return Uma string representando o estado do Gerente.
+     */
+    @Override
+    public String toString() {
+        return "Gerente{" + '}';
+    }
 }
